d4fa13498327d271c6b9d8dd85f9d9a68abe5fa9
someguy <localgitaddress.frame047@passmail.net>
2024-06-06 13:40:24 -0400

    Added Cors config

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const userRoutes = require("./routes/users");
 const articleRoutes = require("./routes/articles");
 const { authenticateJWT } = require("./middleware/auth");
 const { NotFoundError } = require("./expressError");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(authenticateJWT);
 app.use("/users", userRoutes);
 app.use("/articles", articleRoutes);
 
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Reqested-With, Content-Type, Accept, Authorization"
+  );
+
   return next(new NotFoundError());
 });
 
